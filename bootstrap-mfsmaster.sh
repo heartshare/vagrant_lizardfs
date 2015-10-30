3ef7ff289d227703240bb5ead1ec9e303da90ab7
ruilinchu <ruilinchu@gmail.com>
2015-10-30 12:16:49 -0700

    update

--- bootstrap-mfsmaster.sh
+++ bootstrap-mfsmaster.sh
@@ -21,3 +21,16 @@ cat >> /etc/hosts <<EOL
 10.0.15.21  chunk1
 10.0.15.22  chunk2
 EOL
+apt-get install pdsh
+
+cat > /etc/profile.d/pdsh.sh <<EOF
+# setup pdsh for cluster users
+export PDSH_RCMD_TYPE='ssh'
+export WCOLL='/etc/pdsh/machines'
+EOF
+
+mkdir -p /etc/pdsh
+grep 10.0. /etc/hosts | awk '{print $2}' > /etc/pdsh/machines
+
+
+
