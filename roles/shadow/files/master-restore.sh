072516f03c9c35e52f56079367db99ac90da7e95
ruilinchu <ruilinchu@gmail.com>
2015-10-31 19:28:22 -0700

    update

--- roles/shadow/files/master-restore.sh
+++ roles/shadow/files/master-restore.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
-set -x
-if [ -f /var/lib/mfs/metadata_ml.mfs.back -a ! -f /var/lib/mfs/metadata.mfs ];
-then
-    mfsmetarestore -a
-    if [ $? -ne 0 ]; then
-        echo "FAILED to invoke the mfsmetarestore operation, check logs.";
-        echo "Unable to start mfsmaster service.";
-        exit 1;
-    fi;
-fi
-mfsmaster start
+#!/bin/sh
+sed -i 's+PERSONALITY = shadow+PERSONALITY = master+g' /etc/mfs/mfsmaster.cfg
+mfsmaster reload
+
