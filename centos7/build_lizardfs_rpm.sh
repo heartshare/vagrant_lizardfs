40a633cb2ba4b57a662cc84c1a6f811dafd33922
ruilinchu <ruilinchu@gmail.com>
2016-01-13 13:56:47 -0800

    Update build_lizardfs_rpm.sh

--- centos7/build_lizardfs_rpm.sh
+++ centos7/build_lizardfs_rpm.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
- yum install redhat-lsb-core rpm-build gcc gcc-c++ fuse-devel cmake zlib-devel asciidoc git -y
- git clone https://github.com/lizardfs/lizardfs
- cd lizardfs
-./create-rpm-package.sh 
+yum install -y git
+git clone https://github.com/lizardfs/lizardfs
+cd lizardfs
+
+if ls ./lizardfs*.rpm 1> /dev/null 2>&1; then
+ echo "rpm is ready"
+else
+ yum install -y redhat-lsb-core rpm-build gcc gcc-c++ fuse-devel cmake zlib-devel asciidoc git -y
+ ./create-rpm-package.sh 
+fi
 
 
 
